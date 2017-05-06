4afe01fddb827020d290d3d69920287b459cb0e8
meghana <mdparab1695@gmail.com>
2017-05-06 17:20:17 +0530

    hi

---
+++ batch.txt
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd fyp
+cd temp
+git add .
+git commit -am "made changes"
+git push
+echo Press Enter...
+read
---
+++ temp/batch.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git init 
+cd fyp/temp
+git add .
+git commit -am "made changes"
+git push
+echo Press Enter...
+read
